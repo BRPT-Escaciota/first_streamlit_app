6f10d8688850cda040ef2b54f0a2820cc18bc71b
BRPT-Escaciota <63111988+BRPT-Escaciota@users.noreply.github.com>
2022-05-08 12:18:07 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,6 +22,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page
 streamlit.dataframe(fruits_to_show)
 
+streamlit.header('Fruityvice Fruit Advice!')
+
+
 fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
 streamlit.write('The user entered', fruit_choice)
 
@@ -30,8 +33,6 @@ fruityvice_response = requests.get("http://fruityvice.com/api/fruit/" + fruit_ch
 
 # take the json version of the response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-
-streamlit.header('Fruityvice Fruit Advice!')
 
 # outpu it the screen as a table
 streamlit.dataframe(fruityvice_normalized)
